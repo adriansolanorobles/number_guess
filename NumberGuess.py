c424b74ed918028279ce7722145a830fecbe4be6
adriansolanorobles <adrian.solano@protonmail.com>
2017-05-09 16:44:45 -0500

    Adivina

---
+++ NumberGuess.py
@@ -0,0 +1,69 @@
+NumberGuess.py
+"""
+rolls a pair of dice and asks the user to guess a number this should determinate the winner
+"""
+from random import randint
+from time import sleep
+
+def get_user_guess():
+  user_guess = int(raw_input("Guess a number: "))
+  return user_guess
+
+def roll_dice(number_of_sides):
+  first_roll = randint(1,number_of_sides)
+  second_roll = randint(1,number_of_sides)
+  max_val = number_of_sides * 2
+  print "The maximum possible value is: " + str(max_val)
+  sleep(1)
+  user_guess = get_user_guess()
+  if user_guess > max_val:
+    print "No guessing higher than the maximum possible value"
+    return
+  else:
+    print "Rolling..."
+    sleep(2)
+    print "The first value is: %d" % first_roll
+    sleep(1)
+    print "The second value is: %d" % second_roll
+    total_roll = first_roll + second_roll
+    print "The total value is: %d" % total_roll
+    print "Result..."
+    sleep(1)
+    if user_guess > total_roll:
+      print "You won!"
+      return
+    else:
+      print "You lost"
+      return
+
+
+roll_dice(6)
+     
+      
+      
+     
+      
+    
+    
+    
+    
+    
+    
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+  
+  
+  
+
+
